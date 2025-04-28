7c550d2fe2dd6ddc557d5c03cce0f0ca71a6ecce
Jack <jack20220723@gmail.com>
2025-04-28 11:34:07 +0800

    Enhance package name with 'org.qubitpi.ws.notification'

--- src/main/java/org/qubitpi/ws/notification/NotificationWsApplication.java
+++ src/main/java/org/qubitpi/ws/notification/NotificationWsApplication.java
@@ -1,4 +1,4 @@
-package org.qubitpi.notification_ws;
+package org.qubitpi.ws.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/qubitpi/ws/notification/NotificationWsApplicationTests.java
+++ src/test/java/org/qubitpi/ws/notification/NotificationWsApplicationTests.java
@@ -1,4 +1,4 @@
-package org.qubitpi.notification_ws;
+package org.qubitpi.ws.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
