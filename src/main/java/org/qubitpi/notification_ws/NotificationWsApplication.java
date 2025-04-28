4a6096911b6aa12ce39c3de74deba0b4b66b1915
Jack <jack20220723@gmail.com>
2025-04-28 11:28:36 +0800

    Project init using fast-spring with 'curl -G http://localhost:8080/starter.zip -d dependencies=web -d javaVersion=17 -d name=notification-ws -d groupId=org.qubitpi -d artifactId=notification-ws -d packaging=jar -o notification-ws.zip'

---
+++ src/main/java/org/qubitpi/notification_ws/NotificationWsApplication.java
@@ -0,0 +1,13 @@
+package org.qubitpi.notification_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qubitpi/notification_ws/NotificationWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qubitpi.notification_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
